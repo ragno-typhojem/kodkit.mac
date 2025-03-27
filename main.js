6c230519eb4bca0479c5a70ca04eaff12d5ea860
ragno-typhojem <ragnogamescorp@gmail.com>
2025-03-27 18:38:02 +0300

    kodkit is an app now yay

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Build klasöründeki index.html dosyasını yükle
+  win.loadFile(path.join(__dirname, 'build', 'index.html'));
+
+  // Geliştirici araçlarını açmak isterseniz aşağıdaki satırı yorum olmaktan çıkarın
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
